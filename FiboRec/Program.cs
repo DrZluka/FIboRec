839b00eda15fdda761ed01f3553dcbd21039f7bb
Федор-ПК\Федор <curafedor@gmail.com>
2020-12-20 03:56:20 +0200

    Fibonacci

---
+++ FiboRec/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FiboRec
+{
+    class Program
+    {
+        public static int Fibonacci (int n)
+        {
+            int a = 0;
+            int b = 1;
+            int i = 0;
+            while(i < n)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+                i++;
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Количество подсчётов:");
+            int some = int.Parse(Console.ReadLine());
+            int i = 0;
+            while(i < some)
+            {
+                Console.WriteLine(Fibonacci(i));
+                i++;
+            }
+        }
+    }
+}
